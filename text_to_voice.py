5b05172ee0918d203faa024e6fbc5cbc5a8d95a4
ArubaAriz <146959652+ArubaAriz@users.noreply.github.com>
2024-08-17 23:00:07 +0530

    Add files via upload

---
+++ text_to_voice.py
@@ -0,0 +1,19 @@
+from gtts import gTTS
+
+import os
+
+f=open('1.text')
+x=f.read()
+
+# with open('1.text', 'r', encoding='utf-8') as file:
+#     text = file.read()
+
+# Define the language as Hindi
+language = 'hi'
+
+audio = gTTS(text,lang=language,slow=False)
+
+audio.save("1.wav")
+os.system("1.wav")
+
+
