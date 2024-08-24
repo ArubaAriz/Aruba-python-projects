ba596579afeb42b3343469baf37a919785079c78
ArubaAriz <146959652+ArubaAriz@users.noreply.github.com>
2024-08-25 00:51:44 +0530

    Update text_to_voice.py

--- text_to_voice.py
+++ text_to_voice.py
@@ -5,10 +5,11 @@ import os
 f=open('1.text')
 x=f.read()
 
+#for hindi audio conversion
 # with open('1.text', 'r', encoding='utf-8') as file:
 #     text = file.read()
 
-# Define the language as Hindi
+# use hi for language  Hindi and ur for urdu
 language = 'en'
 
 audio = gTTS(text,lang=language,slow=False)
